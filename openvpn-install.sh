a57fa85dcd0138f9b843f915fd62adda46c26255
Alexander Sergevich <alexanderbond1966@gmail.com>
2022-04-24 12:00:59 +0300

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -2,7 +2,6 @@
 #
 #OxideDevX
 
-
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
 	echo "This script needs to be run with bash, not sh"
